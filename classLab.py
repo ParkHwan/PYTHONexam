34aafd1bd423d828a2d734e5cbd7b2a302fee66a
Park Hwan <kanjanggun@naver.com>
2022-02-11 17:40:34 +0900

    add "잔액부족 표시"

--- classLab.py
+++ classLab.py
@@ -4,29 +4,28 @@ class Account:
         self.ownerName = ownerName
         self.balance = balance
 
-    def deposit(self, dep):
-        self.balance += dep
+    def deposit(self, amount):
+        self.balance += amount
 
-    def withdraw(self, wit):
-        self.balance -= wit
+    def withdraw(self, amount):
+        if self.balance < amount:
+            self.balance = " 잔액을 부족합니다"
+        else:
+            self.balance -= amount
+
+def printAccount(obj):
+    print(f"계좌번호 : {obj.accountNo}\n"
+          f"예금주 이름 : {obj.ownerName}\n"
+          f"잔액 : {obj.balance}\n")
 
 obj1 = Account("111-222-3333333", "정수아", 50000)
 obj2 = Account("555-444-9999999", "손이안", 100000)
 
 obj1.deposit(100000)
+printAccount(obj1)
+
 obj2.withdraw(30000)
-
-def printAccount():
-    print(f"계좌번호 : {obj1.accountNo}\n"
-          f"예금주 이름 : {obj1.ownerName}\n"
-          f"잔액 : {obj1.balance}\n\n"
-          f"계좌번호 : {obj2.accountNo}\n"
-          f"예금주 이름 : {obj2.ownerName}\n"
-          f"잔액 : {obj2.balance}\n\n"
-          )
-
-printAccount()
-
+printAccount(obj2)
 
 
 
